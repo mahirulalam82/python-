fecd59ba21313c6d6218f6b2c69a059802186616
md. mahir ul alam <140166039+mahirulalam82@users.noreply.github.com>
2023-09-01 02:10:41 +0600

    Add files via upload

---
+++ multiple.py
@@ -0,0 +1,14 @@
+name = rahim
+age = 30
+men = True 
+name, age, men = "rahim", 30 , True
+print(name)
+print(age)
+print(men)
+
+
+rahim=20
+karim=20
+rahim=karim=20
+print(rahim)
+print(karim)
---
+++ variables.py
@@ -0,0 +1,11 @@
+#Different types of variables
+print(type("hellow"))
+height=248.56
+print(type(height))
+print("my height is:"+str(height)+"cm")
+human=False
+print(type(human)) 
+age=19
+age=age+1
+print(type(age))
+print(age)                                                                                          
