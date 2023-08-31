8591e6530f991815b6b08d7983f0f14bfbc5ee5e
md. mahir ul alam <140166039+mahirulalam82@users.noreply.github.com>
2023-09-01 02:28:48 +0600

    Add files via upload

---
+++ different method.py
@@ -0,0 +1,17 @@
+#length method
+name="mahirul"
+bro="82"
+print(len(name))
+#find method
+print(name.find("i"))
+#capitalize
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(bro.isdigit())
+print(name.isalpha())
+#count how manay character in str
+print(name.count("a"))
+print(name.replace("i","e"))
+print(name*3)
