040149e9c827e80f50193e15a4118353b823c85b
md. mahir ul alam <140166039+mahirulalam82@users.noreply.github.com>
2023-09-01 12:36:17 +0600

    Add files via upload

---
+++ type casting.py
@@ -0,0 +1,9 @@
+x=1
+y=2.1
+z="3"
+y=int(y)
+print(x*3)
+print(int(y))
+print(z*3) #as a string so it shows 333.when we type integer value then it shows multiply value 
+print("x is "+str(x))
+print("y is "+str(y))
