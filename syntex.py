4f1eb66cf83c56433506c382665903ecdf6964ff
md. mahir ul alam <140166039+mahirulalam82@users.noreply.github.com>
2024-06-04 02:12:08 +0600

    Add files via upload

---
+++ syntex.py
@@ -0,0 +1,29 @@
+print("hey Mahir")
+import sys
+print(sys.version)
+if 5 > 2:
+  print("Five is greater than two!")
+#variable input
+x=7 # this is an integar number
+y=6.5 #it's a float number
+if x>y :
+  print ("x is large number")
+#casting
+M=int(5)
+T=int(6.6)
+N=float(7)
+print(M)
+print(N)
+print(T)
+#get data type
+M= "Mahir"
+N=1
+print(type(M))
+print(type(N))
+#multiple variable in one line
+A,B,C="Mahir","rahim","karim"
+print(A,B,C)
+#multiple variable assign in one another variable
+Unit_cell=["SC","BCC","FCC","HCP"]
+G,H,K,L=Unit_cell
+print(G,H,K,L)
